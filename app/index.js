e74e76f2fe4b42d4e0df32b91fe5440801382b0d
opareshbhai <opareshbhai@salesforce.com>
2016-03-20 17:49:15 +0530

    motivation added

--- app/index.js
+++ app/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var motivations = require('motivations');
 
 app.get('/', function(req, res){
-	res.send("NodejsTogether Event is Awesome")
+	res.send(motivations);
 });
+
+app.get('/message', function(req,res){
+	res.send("NodejsTogether Event is Awesome")
+})
 
 module.exports = app;
