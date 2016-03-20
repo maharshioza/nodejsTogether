a9777985fe1616d8503f7367e8ddfe1552ae9b37
opareshbhai <opareshbhai@salesforce.com>
2016-03-20 17:55:16 +0530

    Pick-one used to take random message everytime

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var motivations = require('motivations');
+var pick_one = require('pick-one');
 
 app.get('/', function(req, res){
-	res.send(motivations);
+	res.send(pick_one(motivations));
 });
 
 app.get('/message', function(req,res){
