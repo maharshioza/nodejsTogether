33727421e7bbdf873837c77dab8d8d88c314fd7e
opareshbhai <opareshbhai@salesforce.com>
2016-03-20 17:32:45 +0530

    rules defined in app folder

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var port = '8080';
 var host = '0.0.0.0';
 
 var express = require('express');
-
 var app = express();
+
+app.use(require('./app'));
 
 app.listen(port, host);
 console.log("Server is listening....");
 
--
